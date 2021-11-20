bfbcb5da67025111d62cf2378bf8f13be16b9fbd
Carolyn Johnson Orosz <carolyn.johnson87@gmail.com>
2021-11-20 11:17:10 +0100

    pseudocode and basic structure for ships

--- src/components/Ships/Ships.jsx
+++ src/components/Ships/Ships.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+// TO DO
+// - Header:
+//   - Displays title, description (copied from docs).
+//   - Log out button (routes to login page and removes authentication flag from local storage if it's persisted).
+
+// - Left - Table:
+
+//   - Displays response from the API request https://api.spacexdata.com/v4/ships.
+//   - It should display columns: name, type, active, year built, home port and details column, that contains 'view' button for each record, which functionality will be described below.
+//   - The table should be server-side paginated with 10 results by page. Pagination controls should contain buttons for previous/next page and total number of pages.
+//   - Table should be servier-side sortable by all displayed columns.
+
+// - Right - Details view:
+//   - Displays details about the selected ship from the table, by clicking 'view' button from the details column.
+//   - It should contain ship's name, year built, roles and image if it's available. If an image is not available, the placeholder image should be rendered.
+//   - Displays 'launches' list that contains urls to wikipeadia articles about the launches ship was apart of
+
 const Ships = () => {
-  return <div>Ships Component</div>;
+  return (
+    <>
+      <div>Header component</div>
+      <div>Table with data from space X</div>
+    </>
+  );
 };
 
 export default Ships;
