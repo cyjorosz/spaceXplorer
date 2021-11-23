8929fa14297633f827d2b4ba78b3841c582a9067
Carolyn Johnson Orosz <carolyn.johnson87@gmail.com>
2021-11-23 23:51:05 +0100

    create global styles

---
+++ src/components/Global/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { theme } from './theme';
+
+export const StyledButton = styled.button`
+    background-color: ${theme.colors.pink};
+    border: 1px solid ${theme.colors.pink};
+    border-radius: 4px;
+    font-size: 16px;
+    line-height: 24px;
+    color: white;
+    height: 40px;
+    width: 100%;
+    margin-right: 20px;
+    :hover {
+    cursor: pointer;
+    background: ${theme.colors.gradient};
+    }
+`;
---
+++ src/components/Global/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+  colors: {
+    pink: '#ea4c89',
+    grey: '#F3F3F4',
+    gradient: 'linear-gradient(45deg, #FA7CBB, #F14658)',
+    warning: 'FF0000',
+  },
+};
