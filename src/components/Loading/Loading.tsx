0f6cecb7b272b27e56eed6f3d5cb533de12ac377
Carolyn Johnson Orosz <carolyn.johnson87@gmail.com>
2021-11-23 22:01:54 +0100

    refactored loading to ts and es6 arrow function

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import Loader from 'react-loader-spinner';
 
-function Loading() {
+const Loading = () => {
   return (
     <>
       <Loader type="ThreeDots" color="#ea4c89" height={100} width={100} timeout={3000} />
     </>
   );
-}
+};
 
 export default Loading;
